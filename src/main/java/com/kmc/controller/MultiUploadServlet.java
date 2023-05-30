b5ce746da53d33a7b3d3aa8e64e166f4c4043371
kmc629 <kmcboss7@gmail.com>
2023-05-30 23:46:41 +0900

    파일 업로드

--- src/main/java/com/kmc/controller/MultiUploadServlet.java
+++ src/main/java/com/kmc/controller/MultiUploadServlet.java
@@ -2,6 +2,7 @@ package com.kmc.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -14,16 +15,16 @@ import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 /**
- * Servlet implementation class UploadServlet
+ * Servlet implementation class MultiUploadServlet
  */
-@WebServlet("/upload.do")
-public class UploadServlet extends HttpServlet {
+@WebServlet("/upload2.do")
+public class MultiUploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UploadServlet() {
+    public MultiUploadServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -53,8 +54,6 @@ public class UploadServlet extends HttpServlet {
 		
 		ServletContext context = getServletContext();
 		String uploadFilePath = context.getRealPath(savePath);
-		System.out.println("서버상의 실제 디렉토리 :");
-		System.out.println(uploadFilePath);
 		
 		try {
 			MultipartRequest multi = new MultipartRequest(
@@ -64,19 +63,19 @@ public class UploadServlet extends HttpServlet {
 					encType, //인코딩 방법
 					//동일한 이름이 존재하면 새로운 이름이 부여됨
 					new DefaultFileRenamePolicy());
-			String fileName = multi.getFilesystemName("uploadFile");
-			
-			if(fileName == null) {
-				System.out.print("파일 업로드 되지 않았음");
-			}else {//파일이 업로드 되었을때
-				out.println("<br> 글쓴이 : " +multi.getParameter("name"));
-				out.println("<br> 제 &nbsp; 목 : " +multi.getParameter("title"));
-				out.println("<br> 파일명 : " + fileName);
+			Enumeration files = multi.getFileNames();
+			while(files.hasMoreElements()) {
+				String file = (String) files.nextElement();
+				String file_name = multi.getFilesystemName(file);
+				//중복된 파일을 업로드할 경우 파일명이 바뀐다.
+				String ori_file_name = multi.getOriginalFileName(file);
+				out.println("<br> 업로드된 파일명 : " + file_name);
+				out.println("<br> 원본 파일명 : " + ori_file_name);
+				out.println("<hr>");
 			}//else
 		}catch(Exception e) {
 			System.out.print("예외 발생 : " + e);
 		}//catch
-		
 	}
 
 }
